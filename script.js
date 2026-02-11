16f2877dbb6a618b6572c6ac6d23be7e271982d3
André Mattos <108373894+DezinMS@users.noreply.github.com>
2026-02-11 15:42:04 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const botoes = document.querySelectorAll('button');
-
-botoes.forEach(botao => {
-    botao.addEventListener('click', () => {
-        const produto = botao.parentElement.querySelector('h2').innerText;
-        alert(`${produto} adicionado ao carrinho!`);
-    });
-});
